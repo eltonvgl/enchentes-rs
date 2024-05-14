d325a5e51cbb09af7f9e0d38da831f682786dfba
eltonvgl <44179144+eltonvgl@users.noreply.github.com>
2024-05-14 16:58:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+var frases =[
+    "Imagine uma nova história para sua vida e acredite nela. Paulo Coelho",
+    "Tudo vale a pena quando a alma não é pequena. Fernando Pessoa",
+    "Enquanto houver vontade de lutar haverá esperança de vencer. Santo Agostinho",
+    "Os miseráveis não têm outro remédio a não ser a esperança. William Shakespeare",
+    "A esperança é o sonho do homem acordado. Aristóteles",
+    "No fundo de um buraco ou de um poço, acontece descobrir-se as estrelas. Aristóteles",
+    "E quando acaricio a cabeça de meu cão sei que ele não exige que eu faça sentido ou me explique. Clarice Lispector",
+    "Mais esperança nos meus passos do que tristeza nos meus ombros. Geraldo Eustáquio de Souza",
+    "O amor sem esperança não tem outro refúgio senão a morte. José de Alencar",
+    "Ame a teu próximo como a ti mesmo e não faça aos outros o que não quer que façam contigo. Jesus Cristo.",
+    "Tudo o que a sua mão encontrar para fazer, faça-o com todo o seu coração. Salomão",
+    "Onde há vida, há esperança.",
+    "Nunca houve uma noite, ou um problema que pudesse derrotar o nascer do sol ou a esperança. Bern Williams",
+    "Dois homens olharam através das grades da prisão; um viu a lama, o outro as estrelas. Desconhecido",
+    "Se alguém te der um tapa, oferece o outro lado da face para outro tapa. Jesus Cristo",
+    "Bem aventurados os pacificadores, porque serão chamados filhos de Deus. Jesus Cristo",
+    "Amai-vos uns aos outros, como eu vos amei. Jesus Cristo",
+    "Não temas, eu venci o mundo! Jesus Cristo",
+    "A vida é mais importante do que a comida, e o corpo, mais do que as roupas. Jesus Cristo",
+    "Porque onde estiver o vosso tesouro, aí estará também o vosso coração. Jesus Cristo",
+    "O ladrão não vem senão para roubar, matar e destruir; eu vim para que tenham vida e a tenham em abundância. Jesus Cristo",
+    "Vinde a mim todos vós que estais cansados e eu vos aliviarei, porque meu jugo é suave e o meu fardo é leve. Jesus Cristo",
+    "Eu sou o pão da vida. O que vem a mim jamais terá fome, e o que crê em mim jamais terá sede. Jesus Cristo",
+    "Se vós estiverdes em mim, e as minhas palavras estiverem em vós, pedireis tudo o que quiserdes, e vos será feito. Jesus Cristo",
+    "Das ovelhas que meu Pai me confiou, nenhuma se perderá. Jesus Cristo",
+    "Quem de mim se alimenta por mim viverá.Jesus Cristo",
+    "Eu sou a luz do mundo. Quem me segue, nunca andará em trevas, mas terá a luz da vida. Jesus Cristo",
+    "Nem só de pão viverá o Homem, mas de toda palavra de Deus! Jesus Cristo",
+    "Mil cairão ao teu lado, dez mil à tua direita. Mas tu não serás atingido. Jesus Cristo",
+    "Bem-aventurados os que têm fome e sede de justiça, porque serão fartos. Jesus Cristo",
+    "Portanto, não vos inquieteis com o dia de amanhã, pois o amanhã trará os seus cuidados; basta ao dia o seu próprio mal. Jesus Cristo",
+    "Aquele que crê em mim nunca estará sozinho. Jesus Cristo",
+    "Bem-aventurados os mansos, porque eles herdarão a terra. Jesus Cristo",
+    "Portanto vigiai, porque não sabeis o dia nem a hora em que o Filho do homem há de vir. Jesus Cristo",
+    "A medida do amor é amar sem medida. Santo Agostinho",
+    "Necessitamos um do outro, para sermos nós mesmos. Santo Agostinho",
+    "Ama a Deus sobre todas as coisas. Jesus Cristo",
+    "Pai, perdoa-lhes, porque não sabem o que fazem. Jesus Cristo",
+    "Mas buscai primeiro o Reino de Deus, e a sua Justiça, e as demais coisas vos serão acrescentadas. Jesus Cristo",
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+
+document.getElementById("textoAleatorio").value = frases[Math.floor(Math.random()*frases.length)];
